432a34e1192e7cc72aa4ad3bd550e973f6169c85
prokartem <artem.prokazow@gmail.com>
2023-07-06 10:50:03 +0600

    add install nomad to bootstrap.sh

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -27,6 +27,12 @@ ufw allow https
 echo "y" | ufw enable
 ufw status verbose
 
+echo "============ Install Nomad ============"
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
+apt-get update && apt-get install nomad
+nomad -v
+
 # echo "============ Start Service ============"
 # cd /home/vagrant/great-proxy
 # make render.eth
